28167e5497c016cd5e33744a5de214a68e7f71f5
Noah Zentzis <nzentzis@gmail.com>
2018-01-14 21:26:58 -0800

    Add support for Lisp-based library script

--- src/library/mod.rs
+++ src/library/mod.rs
@@ -7,6 +7,26 @@ mod filesystem;
 mod builtins;
 mod io;
 
+fn run_string(s: &'static str) {
+    use std::io::Cursor;
+    use data::ValueLike;
+    use stream::ReadWrapper;
+    use reader::ParseError;
+
+    let mut cursor = Cursor::new(s);
+    let f = ReadWrapper::new(&mut cursor);
+    let mut env = ::environment::empty();
+    loop {
+        let m = match ::reader::read(&f) {
+            Ok(r) => r,
+            Err(e) => if let ParseError::UnexpectedEOF = e {break;}
+                      else {panic!("Failed to parse Lisp core library")}
+        };
+        m.evaluate(&mut env)
+         .expect("Failed to evaluated Lisp core library form");
+    }
+}
+
 pub fn initialize() {
     core::initialize();
 
@@ -16,4 +36,8 @@ pub fn initialize() {
     async::initialize();
     filesystem::initialize();
     io::initialize();
+
+    // initialize core Lisp library
+    let lib_sources = include_str!("init.ysh");
+    run_string(lib_sources);
 }
